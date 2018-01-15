34e4b7cbc79679dea6080f5d16ea2656674f4b37
Eddy Caldas <eddycaldas@live.com>
2018-01-15 13:13:23 -0700

    up to date with mouse event

--- mouseEvents/app.js
+++ mouseEvents/app.js
@@ -1,4 +1,31 @@
-const clearBtn = document.querySelector('.clean-tasks');
+const clearBtn = document.querySelector('.clear-tasks');
 const card = document.querySelector('.card');
 const heading = document.querySelector('h5');
 
+// click
+clearBtn.addEventListener('click', runEvent);
+// double click
+clearBtn.addEventListener('dblclick', runEvent);
+// Mousedown
+clearBtn.addEventListener('mousedown', runEvent);
+// Mouseup
+clearBtn.addEventListener('mouseup', runEvent);
+// Mouseenter
+card.addEventListener('mouseenter', runEvent);
+// Mouseleave
+card.addEventListener('mouseleave', runEvent);
+// Mouseover
+card.addEventListener('mouseover', runEvent);
+// Mouseout
+card.addEventListener('mouseout', runEvent);
+// mousemove
+card.addEventListener('mousemove', runEvent);
+
+
+// Event Handler
+function runEvent(e){
+    console.log(`EVENT TYPE: ${e.type}`);
+    e.preventDefault();
+    heading.textContent= `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
+    document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
+}
