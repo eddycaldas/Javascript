73fb690c5facfd7fe0de2af0993b3e6bf0338143
Eddy Caldas <eddycaldas@live.com>
2018-01-16 11:14:17 -0700

    input events

--- keyboardAndInputEvents/app.js
+++ keyboardAndInputEvents/app.js
@@ -1,1 +1,19 @@
-console.log("test it")
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
+
+// clear input: taskInput= '';
+
+form.addEventListener('submit', runEvent);
+function runEvent(e){
+    console.log(`EVENT TYPE: ${e.type}`);
+    // get input value
+    console.log(taskInput.value);
+    e.preventDefault();
+}
+
+taskInput.addEventListener('keydown', runEvent)
+function runEvent(e){
+    console.log(e.target.value);
+    heading.innerText = e.target.value;
+}
